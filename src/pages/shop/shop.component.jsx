3c6073640a72cb16202be3084c6e662a611b3ab4
chen605 <chow_chen_1996@hotmail.com>
2021-03-09 12:38:46 +0100

    added loading sequence on shop collection page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,14 +3,23 @@ import { Route } from "react-router-dom";
 import {connect} from "react-redux";
 
 import {UpdateCollections} from "../../redux/shop/shop.actions"
- 
+
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
 import CollectionsOverview from "../../components/collection-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
 
 import {firestore, convertCollectionsSnapshopToMap} from "../../firebase/firebase.utils"
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
@@ -20,19 +29,32 @@ class ShopPage extends React.Component {
     collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshopToMap(snapshot)
       updateCollections(collectionsMap)
+      this.setState({loading: false})
     })
   }
 
-  render(){
-    const {match} = this.props;
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
     return (
-      <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
       </div>
     );
   }
-}  
+} 
 
 const mapDispatchToProps = (dispatch) => ({
   updateCollections: collectionsMap => dispatch(UpdateCollections(collectionsMap))
